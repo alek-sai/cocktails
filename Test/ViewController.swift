3c7eddaf1c4c1e34fd707d7452a8751bf00743c9
Alek Sai <lenyapugachev@me.com>
2022-01-18 22:39:52 +0700

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Alek Sai on 18/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
